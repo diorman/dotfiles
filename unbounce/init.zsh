f8cee5bb5e1bc46373b7a4b711b3d82ac7a14abf
Diorman Colmenares <diorman.c@gmail.com>
2018-02-27 22:58:52 -0800

    Add unbounce config

---
+++ unbounce/init.zsh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+if test -d $HOME/Projects/src/github.com/unbounce/eng-conversions-devtools/bin; then
+	export PATH=$PATH:$HOME/Projects/src/github.com/unbounce/eng-conversions-devtools/bin
+fi
