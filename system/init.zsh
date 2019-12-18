8d4327279e2c21ec496925181fa2a705f4ed5be8
Diorman Colmenares <diorman.c@gmail.com>
2019-12-17 21:13:39 -0500

    Add GNU utilities to PATH

--- system/init.zsh
+++ system/init.zsh
@@ -2,3 +2,16 @@
 
 export PATH=$PATH:~/bin
 export CDPATH=$HOME/Projects/src
+
+# GNU utilities
+for gnupath in "coreutils/libexec/gnubin" \
+	"findutils/libexec/gnubin" \
+	"gnu-tar/libexec/gnubin" \
+	"gnu-sed/libexec/gnubin" \
+  "gawk/libexec/gnubin" \
+	"gnu-indent/libexec/gnubin" \
+	"gnu-getopt/bin" \
+	"grep/libexec/gnubin" \
+  "make/libexec/gnubin"; do
+	export PATH="/usr/local/opt/$gnupath:$PATH"
+done
