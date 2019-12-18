1462c2ddde14c4e34dab3d9b40d1d8882082cc7c
Diorman Colmenares <diorman.c@gmail.com>
2019-12-17 21:50:09 -0500

    Modify script to load nvm lazily

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,15 +1,16 @@
 #!/bin/zsh
 
 if test -d $HOME/.nvm; then
-	__nvm_started=0
 
 	__nvm_init() {
-		test $__nvm_started = 0 && {
-			export NVM_DIR="$HOME/.nvm"
-			[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-			[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-			__nvm_started=1
-		}
+		unset -f nvm
+		unset -f node
+		unset -f npm
+		unset -f __nvm_init
+
+		export NVM_DIR=$HOME/.nvm
+		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 	}
 
 	nvm() {
@@ -19,11 +20,11 @@ if test -d $HOME/.nvm; then
 
 	node() {
 		__nvm_init
-		command node "$@"
+		node "$@"
 	}
 
 	npm() {
 		__nvm_init
-		command npm "$@"
+		npm "$@"
 	}
 fi
