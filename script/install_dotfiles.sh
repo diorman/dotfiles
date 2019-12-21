4c83c6366dbf3ed4e0e514d71e52805c3162b44c
Diorman Colmenares <diorman.c@gmail.com>
2019-12-20 20:48:28 -0500

    Split logging functions into separate file

--- script/install_dotfiles.sh
+++ script/install_dotfiles.sh
@@ -4,31 +4,11 @@
 cd "$(dirname "$0")/.."
 DOTFILES=$(pwd -P)
 
+. $DOTFILES/script/log.inc.sh
+
 set -e
 
 echo ''
-
-info() {
-	# shellcheck disable=SC2059
-	printf "\r  [ \033[00;34m..\033[0m ] $1\n"
-}
-
-user() {
-	# shellcheck disable=SC2059
-	printf "\r  [ \033[0;33m??\033[0m ] $1\n"
-}
-
-success() {
-	# shellcheck disable=SC2059
-	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
-}
-
-fail() {
-	# shellcheck disable=SC2059
-	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-	echo ''
-	exit
-}
 
 link_file() {
 	if [ -e "$2" ]; then
