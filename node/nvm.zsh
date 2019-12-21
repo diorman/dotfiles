23441c37a086858f97c61019985197a1d0fc8d72
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2019-12-20 21:28:13 -0500

    Modify nvm.zsh to use command to invoke commands

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,30 +1,32 @@
 #!/bin/zsh
 
+set -e
+
 if test -d $HOME/.nvm; then
 
 	__nvm_init() {
+		export NVM_DIR=$HOME/.nvm
+		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 		unset -f nvm
 		unset -f node
 		unset -f npm
 		unset -f __nvm_init
-
-		export NVM_DIR=$HOME/.nvm
-		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 	}
 
 	nvm() {
 		__nvm_init
-		nvm "$@"
+		command nvm "$@"
 	}
 
 	node() {
 		__nvm_init
-		node "$@"
+		command node "$@"
 	}
 
 	npm() {
 		__nvm_init
-		npm "$@"
+		command npm "$@"
 	}
 fi
