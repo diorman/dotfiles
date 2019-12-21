9947605af3e3f7e979df1adb117c96e38fcfccd0
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2019-12-20 21:46:48 -0500

    Refactor git-get script

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,11 +4,13 @@ gi() {
 	curl -s "https://www.gitignore.io/api/$*"
 }
 
-gcd() {
-	disable read
-	source $DOTFILES/git/git-get.sh
-	trap "return 0" ERR
-	trap "git_get_cleanup" EXIT
-	git_get_main "$@"
-	cd $(git_get_dest "$@")
-}
+# gcd() {
+# 	disable read
+# 	source $DOTFILES/git/git-get.sh
+# 	trap "return 0" ERR
+# 	trap "git_get_cleanup" EXIT
+# 	git_get_main "$@"
+# 	cd $(git_get_dest "$@")
+# }
+
+alias gcd=". $DOTFILES/bin/git-get"
