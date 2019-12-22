d2fe2ea40cd952035c20b2d7015b17403183e2c5
Diorman Colmenares <diorman.c@gmail.com>
2019-12-21 23:09:47 -0500

    antibody: import functions.sh

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-if ! command -v antibody > /dev/null; then
-	echo 'error: antibody plugin manager not installed'
+
+source $DOTFILES/functions.sh
+
+if command -v antibody > /dev/null; then
+	log_fail "antibody plugin manager not installed"
 	exit 1
 fi
+
 antibody bundle <"$DOTFILES/antibody/plugins.txt" >~/.zsh_plugins.sh
 antibody update
