ff440f7a1c640402011ca61af9c1c518dafaf8e2
Diorman Colmenares <diorman.c@gmail.com>
2019-12-22 02:37:57 -0500

    node: Fix init script

--- node/init.zsh
+++ node/init.zsh
@@ -3,14 +3,14 @@
 if test -d $HOME/.nvm; then
 
 	__nvm_init() {
-		local nvm_dir=$HOME/.nvm
-		[ -s "$nvm_dir/nvm.sh" ] && \. "$nvm_dir/nvm.sh"  # This loads nvm
-		[ -s "$nvm_dir/bash_completion" ] && \. "$nvm_dir/bash_completion"  # This loads nvm bash_completion
-
 		unset -f nvm
 		unset -f node
 		unset -f npm
 		unset -f __nvm_init
+
+		local nvm_dir=$HOME/.nvm
+		[ -s "$nvm_dir/nvm.sh" ] && \. "$nvm_dir/nvm.sh"  # This loads nvm
+		[ -s "$nvm_dir/bash_completion" ] && \. "$nvm_dir/bash_completion"  # This loads nvm bash_completion
 	}
 
 	nvm() {
