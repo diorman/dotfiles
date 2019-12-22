5df7e0443f896fa5e3847c402ded5bc701db76be
Diorman Colmenares <diorman.c@gmail.com>
2019-12-21 23:30:53 -0500

    git: Refactor topic

--- functions.sh
+++ functions.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
-info() {
-	# shellcheck disable=SC2059
+log_info() {
+	printf "\r  [ \033[00;34mINFO\033[0m ] $1\n"
+}
+
+log_init() {
 	printf "\r  [ \033[00;34m..\033[0m ] $1\n"
 }
 
-user() {
-	# shellcheck disable=SC2059
-	printf "\r  [ \033[0;33m??\033[0m ] $1\n"
-}
-
-success() {
-	# shellcheck disable=SC2059
+log_success() {
 	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
 
-fail() {
-	# shellcheck disable=SC2059
+log_fail() {
 	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-	echo ''
-	exit
+}
+
+link_file() {
+	if [ -e "$2" ]; then
+		if [ "$(readlink "$2")" = "$1" ]; then
+			log_success "skipped $1"
+			return 0
+		else
+			mv "$2" "$2.backup"
+			log_success "moved $2 to $2.backup"
+		fi
+	fi
+	ln -sf "$1" "$2"
+	log_success "linked $1 to $2"
 }
