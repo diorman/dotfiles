09f67c245d99a388721d6ee5c7f3ac11eb999fc9
Diorman Colmenares <diorman.c@gmail.com>
2019-12-23 21:13:29 -0500

    Modify dnsmasq installer to reload service

--- dnsmasq/install.sh
+++ dnsmasq/install.sh
@@ -5,3 +5,21 @@ source $DOTFILES/scripts/install-utils.sh
 mkdir -p $(brew --prefix)/etc
 
 link_file "$DOTFILES/dnsmasq/dnsmasq.conf" "$(brew --prefix)/etc/dnsmasq.conf"
+
+svc=($(brew services list | awk '/^dnsmasq/ { printf "%s %s", $2, $3 }'))
+svcstatus="${svc[0]}"
+svcuser="${svc[1]}"
+
+
+if [[ "$svcstatus" == "stopped" ]]; then
+	brew services start dnsmasq
+	exit 0
+fi
+
+# dnsmasq is runinng on port 5353, therefore, sudo is not required
+if [[ "$svcuser" == "root" ]]; then
+	sudo brew services stop dnsmasq
+	brew services start dnsmasq
+else
+	brew services restart dnsmasq
+fi
