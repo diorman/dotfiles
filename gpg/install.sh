cf057ef86c84349c9527a56fcf65a389b150c2d7
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2019-12-23 20:08:13 -0500

    Create ~/.gnupg directory before links

--- gpg/install.sh
+++ gpg/install.sh
@@ -2,5 +2,6 @@
 
 source $DOTFILES/scripts/install-utils.sh
 
+mkdir -p "$HOME/.gnupg"
 link_file "$DOTFILES/gpg/gpg.conf" "$HOME/.gnupg/gpg.conf"
 link_file "$DOTFILES/gpg/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
