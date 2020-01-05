08f587cb99de842e20a7fc9f0598c64110cafdee
Diorman Colmenares <diorman.c@gmail.com>
2020-01-05 16:31:49 -0500

    dnsmasq: Start with sudo listening on port 53

--- dnsmasq/install.sh
+++ dnsmasq/install.sh
@@ -6,20 +6,14 @@ mkdir -p $(brew --prefix)/etc
 
 link_file "$DOTFILES/dnsmasq/dnsmasq.conf" "$(brew --prefix)/etc/dnsmasq.conf"
 
-svc=($(brew services list | awk '/^dnsmasq/ { printf "%s %s", $2, $3 }'))
-svcstatus="${svc[0]}"
-svcuser="${svc[1]}"
+status=($(brew services list | awk '/^dnsmasq/ { printf $2 }'))
 
 
 if [[ "$svcstatus" == "stopped" ]]; then
-	brew services start dnsmasq
+	sudo brew services start dnsmasq
 	exit 0
 fi
 
-# dnsmasq is runinng on port 5353, therefore, sudo is not required
-if [[ "$svcuser" == "root" ]]; then
-	sudo brew services stop dnsmasq
-	brew services start dnsmasq
-else
-	brew services restart dnsmasq
-fi
+# I got some issues with the "restart" command not working properly
+sudo brew services stop dnsmasq
+sudo brew services start dnsmasq
