2bf11e835f96ebca468edd177b50a4c39bcf34ca
Diorman Colmenares <diorman.c@gmail.com>
2020-05-18 10:12:53 -0700

    antibody: Use bootstrap script

--- antibody/bootstrap.sh
+++ antibody/bootstrap.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-source $DOTFILES/scripts/install-utils.sh
+# shellcheck disable=SC1090
+source "$DOTFILES/utils.sh"
 
 if ! command -v antibody > /dev/null; then
-	log_fail "antibody plugin manager not installed"
-	exit 1
+  log_fail "antibody plugin manager not installed"
+  exit 1
 fi
 
 antibody bundle <"$DOTFILES/antibody/plugins.txt" >~/.zsh_plugins.sh
