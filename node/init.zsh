2c30c1e5c35a1cca0713465b0f0f8594ebaf5b5a
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2020-05-29 11:39:35 -0700

    Fix node init script

--- node/init.zsh
+++ node/init.zsh
@@ -2,34 +2,40 @@
 
 if test -d $HOME/.nvm; then
 
-  __nvm_init() {
-    unset -f nvm
-    unset -f node
-    unset -f npm
-    unset -f __nvm_init
+  __nvm_init__() {
+    # unset itself
+    unset -f "${funcstack[1]}"
 
-    local nvm_dir=$HOME/.nvm
+    # unset helper functions
+    for cmd in nvm node npm npx; do
+      unset -f "$cmd"
+    done
+
+    local nvm_dir="$HOME/.nvm"
     [ -s "$nvm_dir/nvm.sh" ] && \. "$nvm_dir/nvm.sh"  # This loads nvm
-    [ -s "$nvm_dir/bash_completion" ] && \. "$nvm_dir/bash_completion"  # This loads nvm bash_completion
+    [ -s "$nvm_dir/bash_completion" ] && \. "$nvm_dir/bash_completion" --no-use # This loads nvm bash_completion
+
+    local node_version
+    local nvmrc_path
+    local nvmrc_node_version
+
+    node_version="$(nvm version)"
+    nvmrc_path="$(nvm_find_nvmrc)"
+
+    if [ -n "$nvmrc_path" ]; then
+      nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+      if [ "$nvmrc_node_version" = "N/A" ]; then
+        nvm install
+      elif [ "$nvmrc_node_version" != "$node_version" ]; then
+        nvm use
+      fi
+    else
+      nvm use default 1>/dev/null
+    fi
   }
 
-  nvm() {
-    __nvm_init
-    command nvm "$@"
-  }
-
-  node() {
-    __nvm_init
-    command node "$@"
-  }
-
-  npm() {
-    __nvm_init
-    command npm "$@"
-  }
-
-  npx() {
-    __nvm_init
-    command npx "$@"
-  }
+  for cmd in nvm node npm npx; do
+    eval "$cmd() { __nvm_init__; command $cmd "\$@"; }"
+  done
 fi
