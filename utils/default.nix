99afa7099491a080c15632b50c444db71c469980
Diorman Colmenares <diorman.c@gmail.com>
2022-03-19 10:51:33 -0400

    More utils

--- utils/default.nix
+++ utils/default.nix
@@ -2,6 +2,9 @@
 
 let
   pj = pkgs.writeScriptBin "pj" "${builtins.readFile ./pj.sh}";
+  gitGet = pkgs.writeScriptBin "git-get" "${builtins.readFile ./git-get.sh}";
+  tab = pkgs.writeScriptBin "tab" "${builtins.readFile ./tab.sh}";
+  tabTree = pkgs.writeScriptBin "tab-tree" "${builtins.readFile ./tab-tree.py}";
 in {
-  home.packages = [ pj ];
+  home.packages = [ pj gitGet tab tabTree ];
 }
