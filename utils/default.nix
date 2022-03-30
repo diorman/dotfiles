bbe176d7f98d6b2e9a6725e278c98e0e547932e8
Diorman Colmenares <diorman.c@gmail.com>
2022-03-30 09:52:25 -0700

    Remove pj utility

--- utils/default.nix
+++ utils/default.nix
@@ -1,9 +1,8 @@
 { pkgs, config, ... }:
 
 let
-  pj = pkgs.writeScriptBin "pj" "${builtins.readFile ./pj.sh}";
   gitGet = pkgs.writeScriptBin "git-get" "${builtins.readFile ./git-get.sh}";
   kittyWindowManager = pkgs.writeScriptBin "kitty-window-manager" "${builtins.readFile ./kitty-window-manager.py}";
 in {
-  home.packages = [ pj gitGet kittyWindowManager ];
+  home.packages = [ gitGet kittyWindowManager ];
 }
