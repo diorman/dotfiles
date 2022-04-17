f3e1a7e82296250d4775cc40fd0d6469aa7c3676
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2022-04-17 14:26:21 -0700

    neovim: LSP support for go + refactor

--- neovim/lua/config/lsp/init.lua
+++ neovim/lua/config/lsp/init.lua
@@ -1,5 +1,48 @@
 require("config.lsp.diagnostic").setup()
-require("config.lsp.lua")
-require("config.lsp.typescript")
-require("config.lsp.nix")
-require("config.lsp.null-ls")
+
+local null_ls = require("null-ls")
+local diagnostics = null_ls.builtins.diagnostics
+local formatting = null_ls.builtins.formatting
+
+null_ls.setup({
+  debug = false,
+  sources = {
+    diagnostics.eslint,
+    formatting.prettierd,
+    formatting.stylua,
+  },
+})
+
+local lspconfig = require("lspconfig")
+local core = require("config.lsp.core")
+local servers = {
+  -- golang
+  gopls = {
+    enabled = true,
+    make_config = core.make_config,
+  },
+
+  -- nix
+  rnix = {
+    enabled = true,
+    make_config = core.make_config,
+  },
+
+  -- lua
+  sumneko_lua = {
+    enabled = true,
+    make_config = require("config.lsp.sumneko-lua").make_config,
+  },
+
+  -- typescript
+  tsserver = {
+    enabled = true,
+    make_config = require("config.lsp.tsserver").make_config,
+  },
+}
+
+for server, settings in pairs(servers) do
+  if settings.enabled then
+    lspconfig[server].setup(settings.make_config())
+  end
+end
--- neovim/lua/config/lsp/nix.lua
+++
@@ -1,2 +0,0 @@
-local config = require("config.lsp.core").make_config()
-require("lspconfig").rnix.setup(config)
