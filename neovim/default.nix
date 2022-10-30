bb7fd98ff3ace9b7d38d3e329e1b9d450b3e49d0
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2022-10-30 14:13:40 -0700

    neovim: Use packer to manage plugins

--- neovim/default.nix
+++ neovim/default.nix
@@ -5,38 +5,7 @@
     enable = true;
     vimAlias = true;
     plugins = with pkgs.vimPlugins; [
-      nvim-web-devicons
-      nvim-tree-lua
-      vim-nix
-      nvim-treesitter
-      nvim-lspconfig
-      lspkind-nvim
-      gitsigns-nvim
-      comment-nvim
-      lualine-nvim
-      nvim-autopairs
-
-      nvim-cmp
-      cmp-buffer
-      cmp-path
-      cmp-nvim-lua
-      cmp-nvim-lsp
-      cmp_luasnip
-      luasnip
-      friendly-snippets
-      null-ls-nvim
-
-      plenary-nvim
-      telescope-nvim
-
-      # Theme
-      nightfox-nvim
-
-      # nvim-buffer-line-lue # tabs
-      # galaxyline-nvim # status bar video: 43:40
-
-      # Indent lines
-      indent-blankline-nvim
+      packer-nvim
     ];
 
     extraConfig = ''
---
+++ neovim/lua/config/packer.lua
@@ -0,0 +1,45 @@
+return require("packer").startup(function(use)
+  use({
+    "nvim-tree/nvim-tree.lua",
+    requires = "nvim-tree/nvim-web-devicons",
+  })
+
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  })
+
+  use("neovim/nvim-lspconfig")
+  use("onsails/lspkind.nvim")
+
+  use("lewis6991/gitsigns.nvim")
+  use("numToStr/Comment.nvim")
+
+  use({
+    "nvim-lualine/lualine.nvim",
+    -- requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  })
+
+  use("windwp/nvim-autopairs")
+
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-nvim-lua")
+  use("hrsh7th/cmp-nvim-lsp")
+  use("saadparwaiz1/cmp_luasnip")
+  use("L3MON4D3/LuaSnip")
+  use("rafamadriz/friendly-snippets")
+  use("jose-elias-alvarez/null-ls.nvim")
+
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = "nvim-lua/plenary.nvim",
+  })
+
+  use("EdenEast/nightfox.nvim")
+
+  use("lukas-reineke/indent-blankline.nvim")
+
+  use("LnL7/vim-nix")
+end)
