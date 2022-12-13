3ab2ade81f61c645a995c4097a6e3d3e20194788
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2022-12-13 03:48:15 -0800

    neovim: Disable Lua's language server 3rd party prompt

--- neovim/lua/config/lsp/sumneko-lua.lua
+++ neovim/lua/config/lsp/sumneko-lua.lua
@@ -22,6 +22,7 @@ M.make_config = function()
         workspace = {
           -- Make the server aware of Neovim runtime files
           library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
         },
         -- Do not send telemetry data containing a randomized but unique identifier
         telemetry = {
