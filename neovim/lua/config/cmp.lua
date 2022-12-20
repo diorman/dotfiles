d24fca5c5e1386caee2a18218b9a11d8a187fd49
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2022-12-20 01:43:47 -0500

    neovim: Address diagnostic warnings

--- neovim/lua/config/cmp.lua
+++ neovim/lua/config/cmp.lua
@@ -12,7 +12,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-Space>"] = cmp.mapping.complete({}),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
@@ -35,5 +35,5 @@ cmp.setup({
   window = {
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
-  }
+  },
 })
