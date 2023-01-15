ce4371fb58846ff931c702d50fb004bf724e6728
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2023-01-15 16:24:02 -0500

    neovim: Set up new keymaps

--- neovim/lua/dc/core.lua
+++ neovim/lua/dc/core.lua
@@ -75,6 +75,32 @@ end
 
 local function setup_keymaps()
   vim.g.mapleader = " "
+
+  vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Selection Down" })
+  vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Selection Up" })
+
+  vim.keymap.set("n", "J", "mzJ`z", { desc = "Same as native J but cursor stays in place" })
+
+  -- Half page jumping with. Cursor stays in the middle
+  vim.keymap.set("n", "<C-d>", "<C-d>zz")
+  vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+  -- Search term stays in the middle
+  vim.keymap.set("n", "n", "nzzzv")
+  vim.keymap.set("n", "N", "Nzzzv")
+
+  -- Deletes selected text and paste contents in buffer
+  vim.keymap.set("x", "<leader>p", [["_dP]])
+  vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+  vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+  vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+  -- Quickfix list navigation
+  vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+  vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+  vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+  vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 end
 
 local function setup_theme()
