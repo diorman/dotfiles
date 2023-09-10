60879f33bb48ee31a2ceaa05610aaea6cf6914b6
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2023-09-09 23:33:15 -0700

    brew: Update casks

--- brew/default.nix
+++ brew/default.nix
@@ -7,18 +7,21 @@
   };
 
   home.file.".Brewfile".text = ''
+    tap "buo/cask-upgrade"
     tap "homebrew/bundle"
     tap "homebrew/cask"
     tap "homebrew/cask-fonts"
     tap "homebrew/cask-versions"
     tap "homebrew/core"
     brew "mas"
+    cask "1password"
     cask "dbeaver-community"
     cask "docker"
     cask "firefox"
     cask "font-fira-code-nerd-font"
     cask "gimp"
     cask "github"
+    cask "karabiner-elements"
     cask "kitty"
     cask "ngrok"
     cask "spotify"
@@ -26,7 +29,6 @@
     cask "visual-studio-code"
     cask "wireshark"
     cask "zoom"
-    mas "1Password 7", id: 1333542190
     mas "Slack", id: 803453959
     mas "Xcode", id: 497799835
   '';
