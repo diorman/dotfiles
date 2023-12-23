afbb502fec1e3a8aaf1175286ffceb8f775c2d51
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2023-12-23 14:55:44 -0500

    kitty: Replace tab groups with os windows

--- kitty/tab_bar.py
+++ kitty/tab_bar.py
@@ -1,49 +1,27 @@
-import sys
-import os
 from kitty.fast_data_types import Screen
-from kitty.tab_bar import DrawData, ExtraData, TabBarData, draw_title
-from kitty.fast_data_types import get_boss
-
-window_manager_path = f'{os.getenv("HOME")}/.config/kitty/window_manager'
-
-if window_manager_path not in sys.path:
-    sys.path.append(window_manager_path)
-
-from windows import get_tab_groups, get_tab_group_key, is_kitten_with_ui_window, get_active_window_in_tab
+from kitty.tab_bar import DrawData, ExtraData, TabBarData
 
 def draw_tab(
     draw_data: DrawData, screen: Screen, tab_bar_data: TabBarData,
     before: int, max_title_length: int, index: int, is_last: bool,
     extra_data: ExtraData
 ) -> int:
-    tab_info = get_tab_info(tab_bar_data.tab_id)
+    caption = []
+    # get command without args. The title always contains the location whether there's an active command or not
+    maybe_command = tab_bar_data.title.split(' ')[0]
+    if maybe_command not in ['exit', 'kitty', 'fish', '~'] and '/' not in maybe_command:
+        caption.append(maybe_command)
 
-    if tab_info:
-        screen.draw(' ' + str(tab_info[2] + 1) + ' ')
-        # screen.cursor.x = screen.cursor.x + 1
+    num_windows = tab_bar_data.num_windows
+
+    if maybe_command == 'kitty':
+        num_windows -= 1
+
+    if num_windows > 1:
+        caption.append(f'[{num_windows}w]')
+
+    title = f'{index}:{" ".join(caption)}' if caption else str(index)
+    screen.draw(' ' + title + ' ')
+    screen.cursor.x += 1
 
     return screen.cursor.x
-
-def get_tab_info(tab_id):
-    boss = get_boss()
-    active_tab = boss.active_tab
-
-    if not boss or not active_tab or is_kitten_with_ui_window(active_tab.active_window):
-        return None
-
-    active_window = get_active_window_in_tab(active_tab)
-
-    if not active_window:
-        return None
-
-    tab_group_key = get_tab_group_key(active_window)
-    tab_group = get_tab_groups(boss, active_tab.os_window_id).get(tab_group_key)
-
-    if not tab_group:
-        return None
-
-    for index, (tab, tab_active_window) in enumerate(tab_group.tabs):
-        if tab.id == tab_id:
-            return (tab, tab_active_window, index)
-
-    return None
