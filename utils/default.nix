56181155b93ee53b8f50dcd581d4c89a998fff56
Diorman Colmenares <diorman.c@gmail.com>
2024-05-09 13:50:01 -0700

    Add extra git utilities

--- utils/default.nix
+++ utils/default.nix
@@ -1,8 +1,11 @@
 { pkgs, config, ... }:
 
 let
+  gitFixup = pkgs.writeScriptBin "git-fixup" "${builtins.readFile ./git-fixup.sh}";
+  gitFuzzySwitch = pkgs.writeScriptBin "git-fuzzy-switch" "${builtins.readFile ./git-fuzzy-switch.sh}";
   gitGet = pkgs.writeScriptBin "git-get" "${builtins.readFile ./git-get.sh}";
   kittyWindowManager = pkgs.writeScriptBin "kitty-window-manager" "${builtins.readFile ./kitty-window-manager.py}";
-in {
-  home.packages = [ gitGet kittyWindowManager ];
+in
+{
+  home.packages = [ gitFixup gitFuzzySwitch gitGet kittyWindowManager ];
 }
