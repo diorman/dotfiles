32252d2d8f38a5cd37d8408a1f4abd491f9dccb4
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2024-06-14 19:31:04 -0700

    osx: Reduce keyboard repeat rate

--- osx.sh
+++ osx.sh
@@ -69,8 +69,8 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 20
 
 # Set language and text formats
 defaults write NSGlobalDomain AppleLanguages -array "en" "es"
